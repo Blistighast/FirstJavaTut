101ae1fc5efc19e06cb7a828b270ef7fdc06cdc5
Brian Guterl <brianguterl99@gmail.com>
2024-08-14 21:14:58 -0400

    2d ArrayList

--- src/ArrayLists.java
+++ src/ArrayLists.java
@@ -6,19 +6,44 @@ public class ArrayLists {
         //elements can be added or removed after compilation
         //store ref data types, must use wrapper class if primitive
 
-        ArrayList<String> food = new ArrayList<String>();
+//        ArrayList<String> food = new ArrayList<String>();
+//
+//        food.add("pizza");
+//        food.add("hamburger");
+//        food.add("hotdog");
+//
+//        for (int i = 0; i < food.size(); i++) {
+//            System.out.println(food.get(i));
+//        }
+//
+//        //methods
+//        food.set(0, "sushi"); //change element at given index
+//        food.remove(1); //remove at given index
+//        food.clear(); //clears array list
 
-        food.add("pizza");
-        food.add("hamburger");
-        food.add("hotdog");
+        //2d ArrayList - dynamic list of lists
 
-        for (int i = 0; i < food.size(); i++) {
-            System.out.println(food.get(i));
-        }
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
 
-        //methods
-        food.set(0, "sushi"); //change element at given index
-        food.remove(1); //remove at given index
-        food.clear(); //clears array list
+        ArrayList<String> bakeryList = new ArrayList<String>();
+        bakeryList.add("pasta");
+        bakeryList.add("bread");
+        bakeryList.add("donuts");
+
+        ArrayList<String> produceList = new ArrayList<String>();
+        produceList.add("tomatoes");
+        produceList.add("cucumber");
+        produceList.add("peppers");
+
+        ArrayList<String> drinksList = new ArrayList<String>();
+        drinksList.add("coffee");
+        drinksList.add("soda");
+
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(drinksList);
+
+        System.out.println(groceryList.get(0).get(0));
+
     }
 }
